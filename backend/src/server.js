54ab0a4dfc5d30a938aac4e58d71e360136b8929
Hunter Nguyen <Hauhonguyen2001@gmail.com>
2025-09-26 23:14:35 +0700

    Setup fullstack deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,24 +3,36 @@ import tasksRoute from "./routes/tasksRouters.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 const app = express();
 
 //middlewares
 app.use(express.json()); // Giúp server hiểu được dữ liệu gửi lên từ client là dạng JSON
-app.use(cors({ origin: "http://localhost:5173" })); // Chỉ cho phép frontend ở cổng 5173 kết nối
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
 
 app.use("/api/tasks", tasksRoute);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+
+// Kết nối DB rồi mới start server
 connectDB().then(() => {
   // Kết nối DB thành công thì mới chạy server
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
 });
-
-
